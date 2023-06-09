5322cbb7ffa1026671272a4cedb76937a90311c8
fanylouise <gandra.fany@gmail.com>
2023-06-09 17:51:49 -0300

    value

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-let textField = document.querySelector('.text');
-let decryptText = document.querySelector('.decryptText');
-let encryptButton = document.querySelector('.encrypt').addEventListener('click', ()=>{
+let output = [];
+
+const letters = ["a","e","i","o","u"];
+const rules = ["ai","enter","imes","ober","ufat"];
+const encryptButton = document.querySelector('.encrypt');
+const decryptButton = document.querySelector('.decrypt');
+const textResult = document.querySelector('.textResult')
+let copy = document.querySelector('.copy');
+let text = document.querySelector('.text');
+
+
+let pegarTexto = ()=>{
+
+output = 
+textResult.innerHTML = text.value.replaceAll(letters[0], rules[0]).replaceAll(letters[4], rules[4]).replaceAll(letters[3], rules[3]).replaceAll('e', 'enter');
+
+
+  }
  
-console.log(decryptText = textField.value.replaceAll('a','ai').replaceAll('e','enter').replaceAll('i','imes').replaceAll('o','ober').replaceAll('u','ufat'))
-});
+  console.log(output)
 
+ encryptButton.addEventListener('mousedown', pegarTexto)
+ copy.addEventListener('mousedown', ClipboardEvent )
 
-let decryptButton = document.querySelector('.decrypt').addEventListener('click',()=>{
-  console.log(textField.value.replaceAll('ai','a').replaceAll('enter','e').replaceAll('imes','i').replaceAll('ober','o').replaceAll('ufat','u'))
-}); 
+//1 Ao clicar no botão 'encrypt';OK
+//2 pegar o texto digitado do textarea;OK
+//2.1 transformar o texto digitado em um novo texto a regra é: todas as letras "a","e","i","o","u" das palavras digitadas devem ser trocadas pelas palavras "ai","enter","imes","ober","ufat";
+//3 Devolver texto encriptado para o campo de resposta
+
